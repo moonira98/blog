9099635df3c94c766ce53eaff0089e0c9a3e3a6e
Munira <mirzoevamunira98@gmail.com>
2025-11-18 16:57:07 +0500

    commit

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from "../../layout/card/card";
+
+@Component({
+  selector: 'app-home',
+  imports: [Card],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
