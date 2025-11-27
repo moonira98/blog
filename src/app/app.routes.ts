50222c9aa6278cfd223b4a69cca0ca95e598ca7c
Munira <mirzoevamunira98@gmail.com>
2025-11-27 16:00:26 +0500

    fonts and resposive design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Details } from './pages/details/details';
+import { LoginAdmin } from './pages/login-admin/login-admin';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: Home},
     {path: 'details/:id', component: Details},
+    {path: 'loginAdmin', component: LoginAdmin}
 ];
